425525a9329f2f63ee1d7c4520781b3dd8216eb2
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-05-15 18:07:54 +0300

    ch2

--- react-pract-1/src/App.js
+++ react-pract-1/src/App.js
@@ -1,42 +1,10 @@
 import './App.css';
-
-function TableOfContents() {
-	return (
-		<section className='tableOfContents'>
-			<h2>Chapter 1. Part 1</h2>
-			<ol>
-				<li>Components: UI Building Blocks</li>
-				<li>Defining a Component</li>
-				<li>Using a Component</li>
-			</ol>
-		</section>
-	);
-}
-
-function ProfilePart() {
-	return (
-		<img
-			src='https://i.imgur.com/MK3eW3As.jpg'
-			alt="Katherine Johnson" />
-	);
-}
-
-function Profile() {
-	return (
-		<section>
-			<h2>Chapter 1. Part 2</h2>
-			<ProfilePart />
-			<ProfilePart />
-			<ProfilePart />
-		</section>
-	);
-}
+import Chapter1 from './chapter1';
 
 function App() {
 	return (
 		<div className="App">
-			<TableOfContents />
-			<Profile />
+			<Chapter1 />
 		</div>
 	);
 }
---
+++ react-pract-1/src/chapter1.js
@@ -0,0 +1,33 @@
+function TableOfContents() {
+	return (
+		<section className='tableOfContents'>
+			<h2>Chapter 1. Part 1</h2>
+			<ol>
+				<li>Components: UI Building Blocks</li>
+				<li>Defining a Component</li>
+				<li>Using a Component</li>
+			</ol>
+		</section>
+	);
+}
+
+function Profile() {
+	return (
+		<img
+			src='https://i.imgur.com/MK3eW3As.jpg'
+			alt="Katherine Johnson" />
+	);
+}
+
+export default function Chapter1() {
+	return (
+		<section>
+			<h2>Chapter 1. Part 1</h2>
+			<TableOfContents />
+			<h2>Chapter 1. Part 2</h2>
+			<Profile />
+			<Profile />
+			<Profile />
+		</section>
+	);
+}
