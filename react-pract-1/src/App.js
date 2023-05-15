b333a6d5693cf200511d0b93626991dfe8aafb58
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-05-15 18:34:25 +0300

    ch2

--- react-pract-1/src/App.js
+++ react-pract-1/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Chapter1 from './chapter1';
 import Chapter2 from './chapter2';
+import { Profile } from './chapter2';
 
 function App() {
 	return (
 		<div className="App">
 			<Chapter1 />
 			<Chapter2 />
+			<h2>Chapter 2. Part 2</h2>
+			<Profile />
 		</div>
 	);
 }
