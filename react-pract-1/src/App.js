ce83b8c614b9c440190836aa8d8f8972706bfdbc
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-05-19 18:37:23 +0300

    ch4

--- react-pract-1/src/App.js
+++ react-pract-1/src/App.js
@@ -3,6 +3,8 @@ import Chapter1 from './chapters/chapter1';
 import Chapter2 from './chapters/chapter2';
 import { Profile } from './chapters/chapter2';
 import Chapter3 from './chapters/chapter3';
+import Chapter4 from './chapters/chapter4';
+
 
 function App() {
 	return (
@@ -14,6 +16,8 @@ function App() {
 			<Profile />
 
 			<Chapter3 />
+
+			<Chapter4 />
 		</div>
 	);
 }
