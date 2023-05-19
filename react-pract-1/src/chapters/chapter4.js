208666a7b7c33143bb030c5ded0edb53fcb689c5
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-05-19 18:59:41 +0300

    ch4

--- react-pract-1/src/chapters/chapter4.js
+++ react-pract-1/src/chapters/chapter4.js
@@ -1,7 +1,36 @@
 export default function Chapter4() {
 	return (
 		<>
-			
+			<h2>Chapter 4, Part 1</h2>
+			<Avatar />
+			<h2>Chapter 4, Part 2</h2>
+			<ToDoList />
 		</>
 	);
+}
+
+function Avatar() {
+	const avatar = "https://i.imgur.com/7vQD0fPs.jpg";
+	const descr = "Gregorio Y. Zara"
+
+	return (
+		<img
+			className="avatar"
+			src={avatar}
+			alt={descr}
+		/>
+	);
+}
+
+function ToDoList() {
+	const name = 'Hedy Lamarr';
+	return (
+		<h1>{name}'s ToDo List for {formatDate(today)}</h1>
+	);
+}
+
+const today = new Date();
+
+function formatDate(date) {
+	return new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(date);
 }
