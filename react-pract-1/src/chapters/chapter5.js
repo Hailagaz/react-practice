f0342ad074ce9f562e4609a8a0e8ebf6e4f4766a
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-05-22 10:12:26 +0300

    ch5

--- react-pract-1/src/chapters/chapter5.js
+++ react-pract-1/src/chapters/chapter5.js
@@ -1,7 +1,31 @@
 export default function Chapter5() {
 	return (
 		<>
-			
+			<br />
+			<h2>Chapter 5. Part 1</h2>
+			<Avatar 
+				person={
+					{
+						name: 'Lin Lanying',
+						imageId: '1bX5QH6'
+					}
+				}
+				size={100}
+			/>
+		</>
+	);
+}
+
+function Avatar({ person, size }) {
+	return (
+		<>
+			<img
+				className='avatar'
+				src="https://i.imgur.com/" + {this.props.person.imageId} + ".jpg"
+				alt={this.props.person.name}
+				width={this.props.size}
+				height={this.props.size}
+			/>
 		</>
 	);
 }
