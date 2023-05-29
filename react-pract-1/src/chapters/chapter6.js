7d1f552e75e0cb43cd93d35a52529c63d1f20304
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-05-29 13:46:49 +0300

    ch6

--- react-pract-1/src/chapters/chapter6.js
+++ react-pract-1/src/chapters/chapter6.js
@@ -1,7 +1,38 @@
 export default function Chapter6() {
 	return (
 		<>
-			
+			<h2>Chapter 6. Part 1</h2>
+			<PackingList />
+		</>
+	);
+}
+
+function PackingList() {
+	return (
+		<section>
+			<h3>Temporary packing list</h3>
+			<ul>
+				<Item 
+					isPacked={true}
+					name="Space suit" 
+				/>
+				<Item
+					isPacked={true}
+					name="Helmet with a golden leaf" 
+				/>
+				<Item
+					isPacked={false}
+					name="Photo of Tam" 
+				/>
+			</ul>
+		</section>
+	);
+}
+
+function Item({name, isPacked}) {
+	return (
+		<>
+			<li className='item'>{name}</li>
 		</>
 	);
 }
