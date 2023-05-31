e429f3c08fc16b9407cf33475bac2c3d715ca4f5
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-05-31 16:54:41 +0300

    ch7

--- react-pract-1/src/chapters/chapter7.js
+++ react-pract-1/src/chapters/chapter7.js
@@ -2,7 +2,25 @@ export default function Chapter7() {
 	return (
 		<>
 			<h2>Chapter 7. Part 1</h2>
-			
+			<List people={listO} />
 		</>
 	);
 }
+
+function List({ people }) {
+	let listItems = people.map(person => <li>{person}</li>)
+	return (
+		<>
+			<p>List of people and their professions</p>
+			{listItems}
+		</>
+	);
+}
+
+let listO = [
+	'Creola Katherine Johnson: mathematician',
+	'Mario José Molina-Pasquel Henríquez: chemist',
+	'Mohammad Abdus Salam: physicist',
+	'Percy Lavon Julian: chemist',
+	'Subrahmanyan Chandrasekhar: astrophysicist'
+];
