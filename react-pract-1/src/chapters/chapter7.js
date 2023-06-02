d2b97eb1c23845b5b844ed92ebaedae1739ab46e
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-06-02 13:23:29 +0300

    ch7

--- react-pract-1/src/chapters/chapter7.js
+++ react-pract-1/src/chapters/chapter7.js
@@ -2,27 +2,40 @@ export default function Chapter7() {
 	return (
 		<>
 			<h2>Chapter 7. Part 1</h2>
-			<List people={listO} />
+			<List1 people={listOne} />
+			<h2>Chapter 7. Part 2</h2>
+			<List2 people={listTwo}/>
 		</>
 	);
 }
 
-function List({ people }) {
+function List1({ people }) {
 	let listItems = people.map(person => <li>{person}</li>)
 	return (
-		<>
+		<div>
 			<p>List of people and their professions</p>
 			<ul>
 				{listItems}
 			</ul>
-		</>
+		</div>
 	);
 }
 
-let listO = [
+function List2({ people }) {
+	return (
+		<div>
+			<p>List of filered people by their profession</p>
+
+		</div>
+	);
+}
+
+let listOne = [
 	'Creola Katherine Johnson: mathematician',
 	'Mario José Molina-Pasquel Henríquez: chemist',
 	'Mohammad Abdus Salam: physicist',
 	'Percy Lavon Julian: chemist',
 	'Subrahmanyan Chandrasekhar: astrophysicist'
 ];
+
+let listTwo = []
