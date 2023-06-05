ba15a9386f03884567fb968cd2ef7866825f10e3
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-06-05 12:33:12 +0300

    ch8

--- react-pract-1/src/chapters/chapter8.js
+++ react-pract-1/src/chapters/chapter8.js
@@ -11,7 +11,16 @@ function Part1() {
 
 	return (
 		<div>
-			<p>Keep component pure all the times</p>
+			<p>Keep component pure all the times.
+				Don't changable variables (change count in props but not by counting).
+				Formula-like behavior</p>
+			<Cup guest={1} />
+			<Cup guest={2} />
+			<Cup guest={3} />
 		</div>
 	);
+}
+
+function Cup({ guest }) {
+	return <h2>Tea cup for guest #{guest}</h2>;
 }
