c4881bb8ebe2670470f8c3a762bfd4dc59e56b7d
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-06-05 10:17:41 +0300

    ch7

--- react-pract-1/src/chapters/chapter7.js
+++ react-pract-1/src/chapters/chapter7.js
@@ -1,6 +1,7 @@
 import { getImageUrl } from "../utils/utils";
 import { listOne } from "../utils/data";
 import { listTwo } from "../utils/data";
+import { recipes } from "../utils/data";
 
 export default function Chapter7() {
 	return (
@@ -9,6 +10,8 @@ export default function Chapter7() {
 			<List1 people={listOne} />
 			<h2>Chapter 7. Part 2</h2>
 			<List2 people={listTwo} />
+			<h2>Chapter 7. Part 3</h2>
+			<RecipeList recipes={recipes} />
 		</>
 	);
 }
@@ -29,14 +32,14 @@ function List2({ people }) {
 
 	let chemists = people.filter(person => person.profession === 'chemist');
 	let listProfsItem = chemists.map(person => <>
-			<li key={person.id}>
-				<img src={getImageUrl(person)} alt={person.name} />
-			</li>
-			<p>
-				<b>{person.name}:</b>
-				{' ' + person.profession + ' '}
-				known for {person.accomplishment}
-			</p>
+		<li key={person.id}>
+			<img src={getImageUrl(person)} alt={person.name} />
+		</li>
+		<p>
+			<b>{person.name}:</b>
+			{' ' + person.profession + ' '}
+			known for {person.accomplishment}
+		</p>
 	</>);
 	return (
 		<div>
@@ -45,5 +48,26 @@ function List2({ people }) {
 				{listProfsItem}
 			</ul>
 		</div>
+	);
+}
+
+function RecipeList({ recipes }) {
+	let result = recipes.map(recipe => 
+		<div key={recipe.id}>
+			<h3>{recipe.name}</h3>
+			<ul>
+				{recipe.ingredients.map(ingredient =>
+					<li key={ingredient}>
+						{ingredient}
+					</li>)}
+			</ul>
+		</div>);
+
+	return (
+		<div>
+			<h3>List of recipes and ingredients</h3>
+			{result}
+		</div>
+
 	);
 }
