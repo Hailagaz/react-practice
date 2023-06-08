8e047ead9326f0f5892146561549025ab8e281fc
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-06-08 13:42:32 +0300

    ch9

--- react-pract-1/src/chapters/chapter9.js
+++ react-pract-1/src/chapters/chapter9.js
@@ -10,6 +10,8 @@ export default function Chapter9() {
 			<Part2 />
 			<h1>Chapter 9. Part 3</h1>
 			<Part3 />
+			<h1>Chapter 9. Part 4</h1>
+			<Part4 />
 		</div>
 	);
 }
@@ -138,4 +140,104 @@ function Counter() {
 			<h1>Score: {score}</h1>
 		</>
 	)
+}
+
+function Part4() {
+	return (
+		<>
+			<FormTwo />
+		</>
+	);
+}
+
+function FormTwo() {
+	const [person, setPerson] = useState({
+		name: 'Niki de Saint Phalle',
+		artwork: {
+			title: 'Blue Nana',
+			city: 'Hamburg',
+			image: 'https://i.imgur.com/Sd1AgUOm.jpg',
+		}
+	});
+
+	function handleNameChange(e) {
+		setPerson({
+			...person,
+			name: e.target.value
+		});
+	}
+
+	function handleTitleChange(e) {
+		setPerson({
+			...person,
+			artwork: {
+				...person.artwork,
+				title: e.target.value
+			}
+		});
+	}
+
+	function handleCityChange(e) {
+		setPerson({
+			...person,
+			artwork: {
+				...person.artwork,
+				city: e.target.value
+			}
+		});
+	}
+
+	function handleImageChange(e) {
+		setPerson({
+			...person,
+			artwork: {
+				...person.artwork,
+				image: e.target.value
+			}
+		});
+	}
+
+	return (
+		<>
+			<label>
+				Name:
+				<input
+					value={person.name}
+					onChange={handleNameChange}
+				/>
+			</label>
+			<label>
+				Title:
+				<input
+					value={person.artwork.title}
+					onChange={handleTitleChange}
+				/>
+			</label>
+			<label>
+				City:
+				<input
+					value={person.artwork.city}
+					onChange={handleCityChange}
+				/>
+			</label>
+			<label>
+				Image:
+				<input
+					value={person.artwork.image}
+					onChange={handleImageChange}
+				/>
+			</label>
+			<p>
+				<i>{person.artwork.title}</i>
+				{' by '}
+				{person.name}
+				<br />
+				(located in {person.artwork.city})
+			</p>
+			<img
+				src={person.artwork.image}
+				alt={person.artwork.title}
+			/>
+		</>
+	);
 }
