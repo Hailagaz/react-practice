c89d73403a4ba93a13d0a1aba5d6ad84b06f9640
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-06-12 12:13:17 +0300

    ch9

--- react-pract-1/src/chapters/chapter9.js
+++ react-pract-1/src/chapters/chapter9.js
@@ -247,7 +247,18 @@ function FormTwo() {
 function Part5() {
 	return (
 		<>
-			
+			<BucketList />
 		</>
 	);
+}
+
+let nextId = 3;
+const initialList = [
+	{ id: 0, title: 'Big Bellies', seen: false},
+	{ id: 1, title: 'Lunar Landscape', seen: false },
+	{ id: 2, title: 'Terracotta Army', seen: true }
+];
+
+function BucketList() {
+
 }
