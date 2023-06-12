3c724f2e61bd79953c4141bfd3d6014a0d927ef4
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-06-12 12:26:11 +0300

    ch9

--- react-pract-1/src/chapters/chapter9.js
+++ react-pract-1/src/chapters/chapter9.js
@@ -254,11 +254,56 @@ function Part5() {
 
 let nextId = 3;
 const initialList = [
-	{ id: 0, title: 'Big Bellies', seen: false},
+	{ id: 0, title: 'Big Bellies', seen: false },
 	{ id: 1, title: 'Lunar Landscape', seen: false },
 	{ id: 2, title: 'Terracotta Army', seen: true }
 ];
 
 function BucketList() {
+	const [list, setList] = useState(initialList);
 
+	function handleToggle(artworkId, nextSeen) {
+		setList(list.map(artwork => {
+			if (artwork.id === artworkId) {
+				return { ...artwork, seen: nextSeen };
+			} else {
+				return artwork;
+			}
+		}));
+	}
+
+	return (
+		<>
+			<h3>Art Bucket List</h3>
+			<h4>My list of art to see:</h4>
+			<ItemList 
+				artworks={list}
+				onToggle={handleToggle}
+			/>
+		</>
+	);
+}
+
+function ItemList({ artworks, onToggle }) {
+	return (
+		<ul>
+			{artworks.map(artwork => (
+				<li key={artwork.id}>
+					<label>
+						<input
+							type="checkbox"
+							checked={artwork.seen}
+							onChange={e => {
+								onToggle(
+									artwork.id,
+									e.target.checked
+								);
+							}}
+						/>
+						{artwork.title}
+					</label>
+				</li>
+			))}
+		</ul>
+	);
 }
