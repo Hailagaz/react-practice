a170d2f15e5ae3842fa29369da3da47e4488260a
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-06-13 14:28:06 +0300

    ch10

--- react-pract-1/src/chapters/chapter10.js
+++ react-pract-1/src/chapters/chapter10.js
@@ -1,4 +1,4 @@
-
+import { useState } from "react";
 
 export default function Chapter10() {
 	return (
@@ -208,6 +208,7 @@ function Part6() {
 	return (
 		<>
 			<LightSwitch />
+			<ColorSwitch />
 		</>
 	);
 }
@@ -228,3 +229,41 @@ function LightSwitch() {
 		</button>
 	);
 }
+
+function ColorSwitch({onChangeColor}) {
+	return (
+		<button onClick={onChangeColor}>
+			Change color
+		</button>
+	);
+}
+
+
+// function Color() {
+// 	const [clicks, setClicks] = useState(0);
+
+// 	function handleClickOutside() {
+// 		setClicks(c => c + 1);
+// 	}
+
+// 	function getRandomLightColor() {
+// 		let r = 150 + Math.round(100 * Math.random());
+// 		let g = 150 + Math.round(100 * Math.random());
+// 		let b = 150 + Math.round(100 * Math.random());
+// 		return `rgb(${r}, ${g}, ${b})`;
+// 	}
+
+// 	function handleChangeColor() {
+// 		let bodyStyle = document.body.style;
+// 		bodyStyle.backgroundColor = getRandomLightColor();
+// 	}
+
+// 	return (
+// 		<div style={{ width: '100%', height: '100%' }} onClick={handleClickOutside}>
+// 			<ColorSwitch onChangeColor={handleChangeColor} />
+// 			<br />
+// 			<br />
+// 			<h2>Clicks on the page: {clicks}</h2>
+// 		</div>
+// 	);
+// }
